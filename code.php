2c17e910351e2d332ac75105eccdc674d34ff2b3
elena778 <kotori122333@gmail.com>
2020-10-15 16:45:40 +0300

    commit

--- code.php
+++ code.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["first_name"]. " -Last Name: " . $row["last_name"]."-Email: ". $row["email"]."-Password:". $row["password"] "<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["first_name"]. " -Last Name: " . $row["last_name"]."-Email: ". $row["email"]."-Password:". $row["password"]. "<br>";
   }
 } else {
   echo "0 results";
