6daea8bd2bb56b1b5be8425e02394cd6f41ad930
elena778 <kotori122333@gmail.com>
2020-10-19 12:52:08 +0300

    commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <body>
-<a href="../signup/controllers/sign_up.php">php</a>
-<form action="code.php" method="post">
+<a href="code.php">php</a>
+<form action="controllers/sign_up.php" method="post">
 Login: <input type="text" name="login"><br>
 Password:<input type="password" name="password">
 <input type="submit" value="Sign Up" name="log_in">
