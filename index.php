52e81a1e36385212bd52385bb38f67b1f9329b00
elena778 <kotori122333@gmail.com>
2020-10-19 12:44:11 +0300

    commit

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <html>
 <body>
 <a href="code.php">php</a>
+<form action="code.php" method="post">
+Login: <input type="text" name="login"><br>
+Password:<input type="password" name="password">
+<input type="submit" value="Sign Up" name="log_in">
+</form>
 </body>
 </html>
