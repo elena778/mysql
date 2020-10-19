125cb24b6604349fd808cf7fec80006c745f8f7f
elena778 <kotori122333@gmail.com>
2020-10-19 12:37:22 +0300

    commit

--- code.php
+++ code.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT persoinid, first_name, last_name, email, password FROM users";
+$sql = "SELECT personid, first_name, last_name, email, password FROM users";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
