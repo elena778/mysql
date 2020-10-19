1c65ad7f337bc9aa0afdb2a7362cad414d176d1d
elena778 <kotori122333@gmail.com>
2020-10-19 12:54:19 +0300

    commit

---
+++ controllers/sign_up.php
@@ -0,0 +1,31 @@
+<?
+$login=name; // $_POST['name']
+$password=pass;
+
+if ($login=="" || $password==""){
+	//print("Empty login or password");
+	redirect("http://localhost/views/sign_up.php",false);
+} 
+
+if ($login!==name || $password!==pass){
+ 	//print("Wrong login or password");
+	redirect('http://localhost/views/sign_up.php',false);
+}
+
+
+if ($_POST["login"] || $_POST["password"]) {
+	redirect('http://localhost',false);
+}
+
+/*$fp=fopen('text.txt','w');
+fwrite($fp,'TEXT');
+fclose($fp);*/
+
+
+function redirect($url, $statusCode = 303)
+{
+   header('Location: ' . $url, true, $statusCode);
+   die();
+}
+
+?>
